a5df8c93ffa30a1cb785ded7fadd1192d624983b
João Paulo <79981086+JoaoPaulo-creator@users.noreply.github.com>
2021-08-18 21:56:26 -0300

    Add files via upload

---
+++ src/cupom_generator.py
@@ -0,0 +1,24 @@
+from random import randint as ri
+
+
+
+letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+l = ''
+numeros = '0123456789'
+n = ''
+
+
+for i in range(0,3):
+    #Randomizando letras
+    randomizar_letras = ri(0, len(letras))
+    l += letras[randomizar_letras: randomizar_letras +1]
+
+
+for p in range(0,2):
+#Randomizando números
+    randomizar_numeros = ri(0, len(numeros))    
+    n += numeros[randomizar_numeros: randomizar_numeros + 1]
+
+
+cupom = l + n
+print (cupom)
